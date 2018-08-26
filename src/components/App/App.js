12c492fbeec0f325dfbb2c830845d34398b11b13
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-08-26 11:36:42 +0200

    create LoginBar component folder and js file

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import LoginBar from '../LoginBar/LoginBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <LoginBar />
+      </div>
+    );
+  }
+}
+
+export default App;

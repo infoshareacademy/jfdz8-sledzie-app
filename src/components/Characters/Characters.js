a6ff91d93b78fbf756b3b6beca8343d7623db5bf
Agata Budzinska <agatabudzinska@gmail.com>
2018-08-26 13:55:15 +0200

    Add Router for each character

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,29 +1,67 @@
 import React, { Component } from 'react';
+import './Characters.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Hero from '../Hero/Hero'
 
 class Characters extends Component {
 
-  state = {
-    characters: []
-  };
-
-
-  componentDidMount () {
-    fetch("https://gateway.marvel.com:443/v1/public/characters?apikey=5b4ca4fb9704024334914e17aabbb5a3")
-      .then(response => response.json())
-      .then(payload => this.setState({characters: payload.data.results}))
-  };
+  // state = {
+  //   characters: []
+  // }
 
   render() {
     return (
-<div>
-  <h1>Characters List</h1>
-  <ul>
-    {this.state.characters.map(character => (
-      <li key={character.id}>{character.name}</li>
-    ))}
-  </ul>
-</div>
+      <Router>
+      <div>
+        <h1>Characters List</h1>
+        <ul>
+          <Link to="/">Main</Link>
+          <Link to="/hero/deadpool">Deadpool</Link>
+          <Link to="/hero/ironman">Iron Man</Link>
+          <Link to="/hero/doctorstrange">Doctor Strange</Link>
+          <Link to="/hero/blackpanther">Black Panther</Link>
+          <Link to="/hero/hulk">Hulk (Bruce Banner)</Link>
+          <Link to="/hero/spiderman">Spider-Man</Link>
+          <Link to="/hero/thor">Thor</Link>
+          <Link to="/hero/thanos">Thanos</Link>
+          <Link to="/hero/captainamerica">Captain America</Link>
+          <Link to="/hero/blackwidow">Black Widow</Link>
 
+          <Route exact path="/" render={() => <p> Main Page</p> } />
+          <Route path="/deadpool"
+                 render={() => <Hero id={1009268}/>}
+          />
+          <Route path="/ironman"
+                 render={() => <Hero id={1009368}/> }
+          />
+          <Route path="/doctorstrange"
+                 render={() => <Hero id={1009282}/>}
+          />
+          <Route path="/blackpanther"
+                 render={() => <Hero id={1009187}/> }
+          />
+          <Route path="/hulk"
+                 render={() =><Hero id={1009351}/> }
+          />
+          <Route path="/spiderman"
+                 render={() => <Hero id={1009610}/> }
+          />
+          <Route path="/thor"
+                 render={() => <Hero id={1009664}/>}
+          />
+          <Route path="/thanos"
+                 render={() => <Hero id={1009652}/> }
+          />
+          <Route path="/captainamerica"
+                 render={() => <Hero id={1009220}/>}
+          />
+          <Route path="/blackwidow"
+                 render={() => <Hero id={1009189}/>}
+          />
+
+        </ul>
+      </div>
+      </Router>
 
     )
   }

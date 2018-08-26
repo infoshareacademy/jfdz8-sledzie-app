5a690bfe2e0a14cac48a4ee82e97524d0e451f65
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-08-26 13:38:34 +0200

    create LoginBar button to open LoginForm

--- src/components/LoginBar/LoginBar.js
+++ src/components/LoginBar/LoginBar.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import './LoginBar.css';
+import logo from './logo.png';
+import LoginForm from '../LoginForm/LoginForm';
 
 class LoginBar extends Component {
+
+  state = {
+    enabled: false
+  }
+
+  handleClick = () => {
+    this.setState({
+      enabled: !this.state.enabled
+    })
+  }
+
   render() {
     return (
-      <p>Test</p>
-
+      <fragment>
+        <div className="login-bar">
+          <button className="btn-sign-in" onClick={this.handleClick}>Zapisz się</button>
+        </div>
+        {this.state.enabled && <LoginForm />}
+        <div className="logo-container">
+          <img className="login-bar-logo" src={logo} alt=""/>
+        </div>
+      </fragment>
     );
   }
 }
---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './LoginForm.css';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div className="login-form">
+
+      </div>
+    );
+  }
+}
+
+export default LoginForm;

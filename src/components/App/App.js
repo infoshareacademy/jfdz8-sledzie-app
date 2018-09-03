2ef823ac09301f3db85457ace2a680980e5c5613
Agata Budzinska <agatabudzinska@gmail.com>
2018-09-03 22:36:41 +0200

    Add background pic on main page & subnav bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import LoginBar from '../LoginBar/LoginBar';
-import Characters from '../Characters/Characters'
+import React, { Component } from 'react'
+import './App.css'
+import LoginBar from '../LoginBar/LoginBar'
+import background from './background.jpg'
+import styled from 'styled-components'
+
+
+const Img = styled.img`
+  width: 100%
+`
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <LoginBar />
-        <Characters />
+        <Img src={background}/>
       </div>
     );
   }
--- src/components/LoginBar/LoginBar.js
+++ src/components/LoginBar/LoginBar.js
@@ -2,6 +2,26 @@ import React, {Component} from 'react';
 import './LoginBar.css';
 import logo from './logo.png';
 import LoginForm from '../LoginForm/LoginForm';
+import Characters from '../Characters/Characters';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const SubmenuBar = styled.nav`
+   background: #3b3b3b;
+   border-top: 1px solid #626262;
+   height: 25px;
+   display: flex;
+   align-items: center;
+`
+
+const StyledLink = styled(Link)`
+  margin-left: 310px
+  color: #fff;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 13px; 
+  font-weight: bolder;  
+`
 
 class LoginBar extends Component {
 
@@ -10,7 +30,7 @@ class LoginBar extends Component {
   }
 
   handleClick = (event) => {
-    event.stopPropagation();
+    event.stopPropagation()
     this.setState({
       enabled: !this.state.enabled
     })
@@ -26,16 +46,26 @@ class LoginBar extends Component {
 
   render() {
     return (
-      <fragment>
+      <React.Fragment>
         <div className="login-bar">
           <button className="btn-sign-in" onClick={this.handleClick}>Zapisz się</button>
+          <SubmenuBar className="submenu-bar">
+            <Router>
+              <div>
+                <StyledLink to="/explore">Poznaj Universum</StyledLink>
+                <Route path="/explore" render={() => <Characters />}/>
+              </div>
+            </Router>
+
+            {/*<Characters />*/}
+          </SubmenuBar>
         </div>
         {this.state.enabled && <LoginForm />}
         <div className="logo-bar-container">
           <img className="login-bar-logo" src={logo} alt=""/>
         </div>
-      </fragment>
-    );
+      </React.Fragment>
+    )
   }
 }
 

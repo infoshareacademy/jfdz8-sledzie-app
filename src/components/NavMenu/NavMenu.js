52af090080b64f33ef69d688efe90616dd20f51e
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-09-09 13:37:40 +0200

    add link to menu

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,21 +1,23 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import { Link } from 'react-router-dom';
 import './NavMenu.css';
+
 
 class NavMenu extends Component {
 
   render() {
     return (
-      <fragment>
+      <Fragment>
         <div className='nav'>
           <ul className='nav-bar'>
-            <li className='nav-bar-item'>menu 1</li>
+            <li className='nav-bar-item'><Link to='/heros'>SUPERBOHATEROWIE</Link></li>
             <li className='nav-bar-item'>menu 2</li>
             <li className='nav-bar-item'>menu 3</li>
             <li className='nav-bar-item'>menu 4</li>
             <li className='nav-bar-item'>menu 5</li>
           </ul>
         </div>
-      </fragment>
+      </Fragment>
     );
   }
 }

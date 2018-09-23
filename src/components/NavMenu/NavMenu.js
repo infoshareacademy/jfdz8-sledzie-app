f957940b9d9286fa23425fb66b60f41fb65d5c39
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-09-23 10:53:59 +0200

    add link to menu

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,21 +1,28 @@
 import React, {Component, Fragment} from 'react';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import Characters from "../Characters/Characters";
 
 
 class NavMenu extends Component {
+  state = {
+    show: false
+  };
 
   render() {
     return (
       <Fragment>
         <div className='nav'>
           <ul className='nav-bar'>
-            <li className='nav-bar-item'><Link to='/heros'>SUPERBOHATEROWIE</Link></li>
+            <li onMouseOver={() => this.setState({show: true})} onMouseLeave={() => this.setState({show: false})} className='nav-bar-item'><Link to='/heros'>SUPERBOHATEROWIE</Link></li>
             <li className='nav-bar-item'>menu 2</li>
             <li className='nav-bar-item'>menu 3</li>
             <li className='nav-bar-item'>menu 4</li>
             <li className='nav-bar-item'>menu 5</li>
           </ul>
+        </div>
+        <div hidden={!this.state.show}>
+          <Characters/>
         </div>
       </Fragment>
     );

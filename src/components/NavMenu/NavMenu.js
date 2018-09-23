41c5ef20ff087fe60f14e94028b8797b72ae8942
piotrnowoslawski <jdm-option@jdm-option.pl>
2018-09-23 12:38:52 +0200

    refactoring

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,30 +1,39 @@
 import React, {Component, Fragment} from 'react';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
-import Characters from "../Characters/Characters";
 
 
 class NavMenu extends Component {
   state = {
-    show: false
+    show: 0,
   };
 
+
   render() {
+    const options = {
+      1: () => <p>Hello</p>,
+      2: () => <p>Blah</p>,
+      3: () => <p>Zp</p>,
+      4: () => <p>aaaaa</p>,
+      5: () => <p>fooo</p>
+    }
     return (
-      <Fragment>
+      <div onMouseLeave={() => this.setState({show: 0})}>
         <div className='nav'>
           <ul className='nav-bar'>
-            <li onMouseOver={() => this.setState({show: true})} onMouseLeave={() => this.setState({show: false})} className='nav-bar-item'><Link to='/heros'>SUPERBOHATEROWIE</Link></li>
-            <li className='nav-bar-item'>menu 2</li>
-            <li className='nav-bar-item'>menu 3</li>
-            <li className='nav-bar-item'>menu 4</li>
-            <li className='nav-bar-item'>menu 5</li>
+            <li onMouseOver={() => this.setState({show: 1})} className='nav-bar-item'><Link to='/heroes'>SUPERBOHATEROWIE</Link></li>
+            <li onMouseOver={() => this.setState({show: 2})} className='nav-bar-item'>menu 2</li>
+            <li onMouseOver={() => this.setState({show: 3})} className='nav-bar-item'>menu 3</li>
+            <li onMouseOver={() => this.setState({show: 4})} className='nav-bar-item'>menu 4</li>
+            <li onMouseOver={() => this.setState({show: 5})} className='nav-bar-item'>menu 5</li>
           </ul>
         </div>
-        <div hidden={!this.state.show}>
-          <Characters/>
+        <div>
+          <div>
+            {options[this.state.show] && options[this.state.show]()}
+          </div>
         </div>
-      </Fragment>
+      </div>
     );
   }
 }
